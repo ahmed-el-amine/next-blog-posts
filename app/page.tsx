6aabe70e22338b87f4ce76a1ee2ea81ecb707017
ahmed <ahmedmohmed12363@yahoo.com>
2025-05-14 12:31:31 +0300

    feat(ui): add shadcn/ui components and update dependencies
    Added shadcn/ui components including Button, Sheet, NavigationMenu, and updated dependencies to support the new UI library. Removed unused files and refactored global styles to align with the new design system.

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,9 @@
-import { auth, signIn, signOut } from "@/lib/auth";
-import { AuthError } from "next-auth";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
     <div>
-      {JSON.stringify(session?.user)}
-      <form
-        action={async () => {
-          "use server";
-          try {
-            await signIn("github");
-          } catch (error) {
-            console.log(error);
-          }
-        }}
-      >
-        <button type="submit">Signin with GitHub</button>
-      </form>
-
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Signin Out</button>
-      </form>
-      <br />
-      <form
-        action={async (formData) => {
-          "use server";
-          await signIn("credentials", formData);
-        }}
-      >
-        <label>
-          Email
-          <input name="email" type="email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" />
-        </label>
-        <button>Sign In</button>
-      </form>
+      <Button>Sign in</Button>
     </div>
   );
 }
